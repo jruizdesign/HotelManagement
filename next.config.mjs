d72a100e43af3f5744ba743e70c962a6330365fd
jruizdesign <jruizdesign@gmail.com>
2025-12-30 02:44:22 +0000

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  postcss: {
+    plugins: [
+      'tailwindcss',
+      'autoprefixer',
+    ],
+  },
 };
 
 export default nextConfig;
